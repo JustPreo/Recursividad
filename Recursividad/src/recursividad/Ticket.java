b21030623083e3f50a8d2e384b4da9463b5b5482
JustPreo <impre0hn@gmail.com>
2025-07-25 08:20:45 -0600

    1

---
+++ Recursividad/src/recursividad/Ticket.java
@@ -0,0 +1,29 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursividad;
+
+/**
+ *
+ * @author user
+ */
+public class Ticket {
+    
+    /*
+    Nombre del pasajero.
+    Monto final pagado por el ticket.
+    Monto original del ticket (antes del descuento).
+    Indicación si el nombre fue reconocido como palíndromo.
+    
+    Inicializa todos los atributos en base al nombre ingresado y el cálculo del sistema.
+    
+    getName(): Devuelve el nombre del pasajero.
+    getFinalAmount(): Devuelve el monto final pagado.
+    getOriginalAmount(): Devuelve el monto original del ticket.
+    isPalindrome(): Indica si el pasajero recibió descuento por nombre palíndromo.
+    print(): Imprime o muestra en la interfaz los datos del ticket: nombre, monto original, monto pagado, y si aplicó descuento.
+    
+    */
+    
+}

df792dc65004df742141f304bb9f69e132425ff6
Edwin Portillo <weaselssh@gmail.com>
2025-07-25 08:58:23 -0600

    arreglos menores

--- Recursividad/src/recursividad/PalindromoAir.java
+++ Recursividad/src/recursividad/PalindromoAir.java
@@ -1,11 +1,19 @@
 package recursividad;
 
 public class PalindromoAir {
-    private Ticket tickets[] = new Ticket[29];
+    private Ticket tickets[] = new Ticket[30];
     
     public boolean isPalindrome(String name) {
-        return false;
+        int i = 0;
+        
+        String nombreEvaluar = tickets[i].getName();
+        int tamanioNombre = nombreEvaluar.length();
+        
+        if (nombreEvaluar.charAt(i) != nombreEvaluar.charAt(tamanioNombre - i)){
+            return false;
+        }
+        
+        return true;
     }
-    
     
 }

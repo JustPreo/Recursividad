bb92981e660f2e2611b6dd1a02f2376d26028bdc
Edwin Portillo <weaselssh@gmail.com>
2025-07-25 08:44:32 -0600

    ahora es public

--- Recursividad/src/recursividad/PalindromoAir.java
+++ Recursividad/src/recursividad/PalindromoAir.java
@@ -3,7 +3,9 @@ package recursividad;
 public class PalindromoAir {
     private Ticket tickets[] = new Ticket[29];
     
-    private boolean isPalindrome(String name) {
+    public boolean isPalindrome(String name) {
         return false;
     }
+    
+    
 }
